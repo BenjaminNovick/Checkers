d4d727e7e9d81bb13f95172f336116e713ad8735
Aryeh Berman <amberman@mail.yu.edu>
2022-05-02 17:03:46 -0400

    Fix Board and BoardImpl

--- src/main/java/Checkers/Board_and_Controller/Board.java
+++ src/main/java/Checkers/Board_and_Controller/Board.java
@@ -3,10 +3,10 @@ package Checkers.Board_and_Controller;
 import java.util.Set;
 
 public interface Board {
-    int getBoardValue();
-    Piece getPiece(int xLocation, int yLocation);
+    double getBoardValue();
+    Piece getPiece(int[] location);
     boolean deletePiece(Piece piece);
-    boolean putPiece();
+    boolean putPiece(Piece piece, int[] location);
     Piece[][] getBoardCopy();
     void setBoard(Piece[][] setup);
     Set<Piece> getAllPieces();
--- src/main/java/Checkers/Board_and_Controller/impl/BoardImpl.java
+++ src/main/java/Checkers/Board_and_Controller/impl/BoardImpl.java
@@ -3,41 +3,103 @@ package Checkers.Board_and_Controller.impl;
 import Checkers.Board_and_Controller.Board;
 import Checkers.Board_and_Controller.Piece;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class BoardImpl implements Board {
+
+    private Piece[][] board;
+    private Set<Piece> pieces;
+
+    public BoardImpl(){
+        this.board = new Piece[8][8];
+        this.pieces = new HashSet<>();
+        for(int i = 0; i < 8; i++){
+            for(int j = 0; j < 8; j++){
+                if(i%2 != j%2){
+                    continue;
+                }
+                if(j < 3){
+                    Piece add = new PieceImpl(Piece.Color.RED, new int[]{i,j}, false);
+                    this.board[i][j] = add;
+                    this.pieces.add(add);
+                }
+                if(j>4){
+                    Piece add = new PieceImpl(Piece.Color.BLACK, new int[]{i,j}, false);
+                    this.board[i][j] = add;
+                    this.pieces.add(add);
+                }
+            }
+        }
+    }
+
     @Override
-    public int getBoardValue() {
+    public double getBoardValue() {
         return 0;
     }
 
     @Override
-    public Piece getPiece(int xLocation, int yLocation) {
-        return null;
+    public Piece getPiece(int[] location) {
+        return this.board[location[0]][location[1]];
     }
 
     @Override
     public boolean deletePiece(Piece piece) {
-        return false;
+        if(!this.pieces.remove(piece)){
+            return false;
+        }
+        this.board[piece.getLocation()[0]][piece.getLocation()[1]] = null;
+        return true;
     }
 
     @Override
-    public boolean putPiece() {
-        return false;
+    public boolean putPiece(Piece piece, int[] location) {
+        if(this.board[location[0]][location[1]] != null){
+            return false;
+        }
+        if((piece.isRed() && location[1] == 7) || (!piece.isRed() && location[1] == 0)){
+            Piece.Color color = Piece.Color.BLACK;
+            if(piece.isRed()){
+                color = Piece.Color.RED;
+            }
+            Piece king = new PieceImpl(color, piece.getLocation(), true);
+            piece = king;
+        }
+        this.board[location[0]][location[1]] = piece;
+        this.pieces.add(piece);
+        piece.movePiece(location);
+        return true;
     }
 
     @Override
     public Piece[][] getBoardCopy() {
-        return new Piece[0][0];
+        return this.board.clone();
     }
 
     @Override
     public void setBoard(Piece[][] setup) {
+        this.board = setup;
+        maintenance();
+    }
 
+    private void maintenance() {
+        this.pieces = new HashSet<>();
+        for(int i = 0; i < 8; i++){
+            for(int j = 0; j < 8; j++){
+                if(this.board[i][j] == null){
+                    continue;
+                }
+                else{
+                    this.board[i][j].movePiece(new int[]{i,j});
+                    this.pieces.add(this.board[i][j]);
+                }
+            }
+        }
     }
 
     @Override
     public Set<Piece> getAllPieces() {
-        return null;
+        return this.pieces;
     }
+
 }
