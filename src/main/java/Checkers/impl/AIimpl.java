426516603f46e532d3e1f9351777caef5aacefb2
Benajmin Novick <Bnovick2@mail.yu.edu>
2022-05-03 18:11:45 -0400

    made more effesent and faster

--- src/main/java/Checkers/impl/AIimpl.java
+++ src/main/java/Checkers/impl/AIimpl.java
@@ -19,7 +19,7 @@ public class AIimpl implements AI {
         List<Move> posiblemoves= controller.GetAllRedMoves();
         Move hightsmove =new Moveimpl(new int[]{1, 1}, new int[]{1, 1},-3);
         for(Move m :posiblemoves){
-            m.setMoveScore(getMoveRedValue(m,level));
+            m.setMoveScore(getMoveRedValue(m,level,hightsmove.getMoveScore()));
             if(m.getMoveScore() > hightsmove.getMoveScore()){
                 hightsmove=m;
             }
@@ -33,41 +33,43 @@ public class AIimpl implements AI {
 
 
         List<Move> posiblemoves= controller.GetAllBlackMoves();
-        Move hightsmove =new Moveimpl(new int[]{1, 1}, new int[]{1, 1},3);
+        Move lowsmove =new Moveimpl(new int[]{1, 1}, new int[]{1, 1},3);
         for(Move m :posiblemoves){
-            m.setMoveScore(getMoveBlackValue(m,level));
-            if(m.getMoveScore() < hightsmove.getMoveScore()){
-                hightsmove=m;
+            m.setMoveScore(getMoveBlackValue(m,level,lowsmove.getMoveScore()));
+            if(m.getMoveScore() < lowsmove.getMoveScore()){
+                lowsmove=m;
             }
         }
 
-        return hightsmove;
+        return lowsmove;
     }
 
 
 
-    private double getMoveRedValue(Move m,int lev) {   // red move is eather defined by the bourd value or bord vale after black does the best move for black
+    private double getMoveRedValue(Move m,int lev,double lastHighst) {   // red move is eather defined by the bourd value or bord vale after black does the best move for black
         if (lev == 0) {
             return board.getBoardValue();
         }
-        Move hightsmove = new Moveimpl(new int[]{1, 1}, new int[]{1, 1}, 3);
+        Move lowsmove = new Moveimpl(new int[]{1, 1}, new int[]{1, 1}, 3);
         controller.DoRedMove(m);
         List<Move> posiblemoves = controller.GetAllBlackMoves();
         if (posiblemoves.isEmpty()) {
             return -1;
         }
         for (Move mo : posiblemoves) {
-            mo.setMoveScore(getMoveBlackValue(mo, level-1));
-            if (mo.getMoveScore() < hightsmove.getMoveScore()) {
-                hightsmove = mo;
+            mo.setMoveScore(getMoveBlackValue(mo, level-1,lowsmove.getMoveScore()));
+            if (mo.getMoveScore() < lowsmove.getMoveScore()) {
+                lowsmove = mo;
             }
-
+            if(lowsmove.getMoveScore()<lastHighst){
+                break;
+            }
 
         }
         controller.Undo();
-        return hightsmove.getMoveScore();
+        return lowsmove.getMoveScore();
     }
-    private double getMoveBlackValue(Move m,int lev){
+    private double getMoveBlackValue(Move m,int lev,double lastlowest){
         if (lev == 0) { // were not loking any furthere
             return board.getBoardValue();
         }
@@ -78,9 +80,12 @@ public class AIimpl implements AI {
             return 1;
         }
         for (Move mo : posiblemoves) {
-            mo.setMoveScore(getMoveBlackValue(mo, level-1));
+            mo.setMoveScore(getMoveRedValue(mo, level-1,hightsmove.getMoveScore()));
             if (mo.getMoveScore() > hightsmove.getMoveScore()) {
                 hightsmove = mo;
+            }
+            if(hightsmove.getMoveScore()>lastlowest){
+                break;
             }
         }
         controller.Undo();
